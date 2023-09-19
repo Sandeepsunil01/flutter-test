cdca20a86ed85b1c9e58f0a4e2c596205f8004da
Sandeep DK <sandeepsunil01@gmail.com>
2023-09-19 17:23:36 +0530

    Grouped Test Cases

--- test/matchs_util_test.dart
+++ test/matchs_util_test.dart
@@ -2,39 +2,42 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_testing/maths_utils.dart';
 
 void main() {
-  test("Check for 2 number addition", () {
-    // Arrange
-    int a = 10;
-    int b = 10;
+  // I have used group
+  group("This performs simple test cases written in match_util file", () {
+    test("Check for 2 number addition", () {
+      // Arrange
+      int a = 10;
+      int b = 10;
 
-    // Act
-    int sum = add(a, b);
+      // Act
+      int sum = add(a, b);
 
-    // Assert
-    expect(sum, 20);
-  });
+      // Assert
+      expect(sum, 20);
+    });
 
-  test("Check for 2 number Substract ", () {
-    // Arrange
-    int a = 20;
-    int b = 10;
+    test("Check for 2 number Substract ", () {
+      // Arrange
+      int a = 20;
+      int b = 10;
 
-    // Act
-    int difference = subtract(a, b);
+      // Act
+      int difference = subtract(a, b);
 
-    // Assert
-    expect(difference, 10);
-  });
+      // Assert
+      expect(difference, 10);
+    });
 
-  test("Check for 2 number multiplication", () {
-    // Arrange
-    int a = 10;
-    int b = 10;
+    test("Check for 2 number multiplication", () {
+      // Arrange
+      int a = 10;
+      int b = 10;
 
-    // Act
-    int value = multiply(a, b);
+      // Act
+      int value = multiply(a, b);
 
-    // Assert
-    expect(value, 100);
+      // Assert
+      expect(value, 100);
+    });
   });
 }
