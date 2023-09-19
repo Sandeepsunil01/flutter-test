66fa428de1b4c6e63a266e63dae24b3689e4b626
Sandeep DK <sandeepsunil01@gmail.com>
2023-09-19 17:19:09 +0530

    Basic Test Case for Simple Match Calculation

---
+++ test/matchs_util_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_testing/maths_utils.dart';
+
+void main() {
+  test("Check for 2 number addition", () {
+    // Arrange
+    int a = 10;
+    int b = 10;
+
+    // Act
+    int sum = add(a, b);
+
+    // Assert
+    expect(sum, 20);
+  });
+
+  test("Check for 2 number Substract ", () {
+    // Arrange
+    int a = 20;
+    int b = 10;
+
+    // Act
+    int difference = subtract(a, b);
+
+    // Assert
+    expect(difference, 10);
+  });
+
+  test("Check for 2 number multiplication", () {
+    // Arrange
+    int a = 10;
+    int b = 10;
+
+    // Act
+    int value = multiply(a, b);
+
+    // Assert
+    expect(value, 100);
+  });
+}
